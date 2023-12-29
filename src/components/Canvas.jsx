9d10336a8d5f25e1d5f93e6a78a6c34cac2d69bf
banuka <banukaramanayaka76@gmail.com>
2023-12-29 16:14:44 +0530

    feat:image seperated to grids without pixels

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { ReactP5Wrapper } from "@p5-wrapper/react";
 let img;
-let graphic;
+
 let cw = 400;
 let ch = 400;
 let cellSize = 20;
@@ -11,10 +11,7 @@ let cells = [];
 export default function Canvas(props) {
   return (
     <div>
-      <div
-
-      // style={{ width: "100%", height: "100%" }}
-      >
+      <div>
         <ReactP5Wrapper sketch={sketch} />
       </div>
     </div>
@@ -30,36 +27,41 @@ function sketch(p5) {
 function setup(p5) {
   return () => {
     p5.createCanvas(cw, ch, { willReadFrequently: true });
-    graphic = p5.createGraphics(200, 200);
+
     p5.background(0);
-    graphic.image(img, 0, 0);
-    graphic.loadPixels();
+
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        cells.push({
+          x: x * cellSize,
+          y: y * cellSize,
+        });
+      }
+    }
   };
 }
 function preload(p5) {
   img = p5.loadImage("./face.jpg");
 }
 function draw(p5) {
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      cells.push({
-        x: x * cellSize,
-        y: y * cellSize,
-        size: cellSize,
-        color: Math.random() * 255,
-      });
-    }
-  }
   return () => {
-    cells.forEach((cell) => {
+    cells.forEach((cell, i) => {
+      const x = Math.floor(cell.x / cellSize);
+      const y = Math.floor(cell.y / cellSize);
       p5.push();
-      p5.fill(cell.color);
-      p5.translate(cell.x, cell.y);
-      p5.square(0, 0, cell.size);
-      p5.pop();
+
+      p5.image(
+        img,
+        cell.x,
+        cell.y,
+        cellSize,
+        cellSize,
+        x * cellSize,
+        y * cols,
+        cellSize,
+        cellSize
+      );
     });
   };
 }
-function mousePressed(p5) {
-  console.log(graphic);
-}
+function mousePressed(p5) {}
---
+++ src/components/Canvas1.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useRef, useState } from "react";
+import { ReactP5Wrapper } from "@p5-wrapper/react";
+let img;
+let graphic;
+let cw = 400;
+let ch = 400;
+let cellSize = 20;
+let rows = cw / cellSize;
+let cols = ch / cellSize;
+let cells = [];
+export default function Canvas(props) {
+  return (
+    <div>
+      <div
+
+      // style={{ width: "100%", height: "100%" }}
+      >
+        <ReactP5Wrapper sketch={sketch} />
+      </div>
+    </div>
+  );
+}
+
+function sketch(p5) {
+  p5.preload = preload(p5);
+  p5.setup = setup(p5);
+  p5.draw = draw(p5);
+  p5.mousePressed = () => mousePressed(p5);
+}
+function setup(p5) {
+  return () => {
+    p5.createCanvas(cw, ch, { willReadFrequently: true });
+    graphic = p5.createGraphics(200, 200);
+    p5.background(0);
+    graphic.image(img, 0, 0);
+    graphic.loadPixels();
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        cells.push({
+          x: x * cellSize,
+          y: y * cellSize,
+          size: cellSize,
+          color: Math.random() * 255,
+        });
+      }
+    }
+  };
+}
+function preload(p5) {
+  img = p5.loadImage("./face.jpg");
+}
+function draw(p5) {
+  return () => {
+    for (let i = 100; i < 300; i++) {
+      const x = Math.floor(cells[i].x / cellSize);
+      const y = Math.floor(cells[i].y / cellSize);
+      p5.push();
+      p5.image(
+        img,
+        cells[i].x,
+        cells[i].y,
+        cellSize,
+        cellSize,
+        x * cellSize,
+        y * cols,
+        cellSize,
+        cellSize
+      );
+      p5.pop();
+    }
+
+    // cells.forEach((cell, i) => {
+    //   const x = Math.floor(cell.x / cellSize);
+    //   const y = Math.floor(cell.y / cellSize);
+    //   p5.push();
+    //   // p5.fill(cell.color);
+    //   // p5.translate(cell.x, cell.y);
+    //   p5.image(
+    //     img,
+    //     cell.x,
+    //     cell.y,
+    //     cellSize,
+    //     cellSize,
+    //     x * cellSize,
+    //     y * cols,
+    //     cellSize,
+    //     cellSize
+    //   );
+
+    //   // p5.Fill(0, 0, 0, 0.5);
+    //   // p5.strokeWeight(5);
+    //   // p5.stroke(255);
+    //   // p5.square(cell.x, cell.y, cellSize);
+    //   p5.pop();
+    // });
+  };
+}
+function mousePressed(p5) {
+  console.log(graphic);
+}
